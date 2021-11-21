6d90513b8502a94d0f98cfeff635267fc20bc553
SGDeveloperAcct <sarandevi75@gmail.com>
2021-11-21 16:32:47 +0530

    first set of changes

--- README.md
+++ README.md
@@ -1,3 +1,36 @@
-# angular-ivy-aprpi7
+# RxJS
+
+- Creation operators
+
+  - from
+    - Creates an Observable from an Array, an array-like object, a Promise, an iterable object, or an Observable-like object.
+  - of
+    - Converts the arguments to an observable sequence.
+    * Each argument becomes a next notification.
+  - fromEvent
+    - Creates an Observable that emits events of a specific type coming from the given event target.
+  - interval
+    - Creates an Observable that emits sequential numbers every specified interval of time, on a specified SchedulerLike.
+  - range
+    - Creates an Observable that emits a sequence of numbers within a specified range.
+
+- Join operators
+
+  - concat
+  -
+
+- Filter operators
+
+  - Filter
+  - take
+    - Emits only the first count values emitted by the source Observable.
+  - takeUntil
+
+- Transformation operations
+
+  - map
+
+* Others
+  - tap
 
 [Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-aprpi7)
--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -1,10 +1,43 @@
 import { Component, Input } from '@angular/core';
+import { from, of, interval, fromEvent, range } from 'rxjs';
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'hello',
   template: `<h1>Hello {{name}}!</h1>`,
-  styles: [`h1 { font-family: Lato; }`]
+  styles: [`h1 { font-family: Lato; }`],
 })
-export class HelloComponent  {
+export class HelloComponent {
   @Input() name: string;
+
+  constructor() {
+    this.creationalOperators();
+  }
+
+  creationalOperators() {
+    console.log('from start');
+    const from$ = from([1, 2, 3]);
+    from$.subscribe((k) => console.log(k));
+    console.log('from end');
+
+    console.log('of start');
+    const of$ = of(1, '2', { name: 'test' }, [4, 5, 6]);
+    of$.subscribe((k) => console.log(k));
+    console.log('of end');
+
+    console.log('interval & take start');
+    const interval$ = interval(1000).pipe(take(10));
+    interval$.subscribe((k) => console.log(k));
+    console.log('interval & take end');
+
+    console.log('fromEvent start');
+    const fromEvent$ = fromEvent(document, 'click').pipe(take(5));
+    fromEvent$.subscribe((k) => console.log(k));
+    console.log('fromEvent end');
+
+    console.log('range start');
+    const range$ = range(10, 13);
+    range$.subscribe((k) => console.log(k));
+    console.log('range end');
+  }
 }
